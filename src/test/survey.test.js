4eb4209f573a75e97a261cd4a6fc745bd6fa6ebe
Jaybee <jbalog@village88.com>
2021-03-02 20:34:14 +0800

    test commit 1

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -7,7 +7,7 @@ const SurveyModel           = require('../models/survey.model');
 
 describe("Survey Model", function(){
 
-    it('Should return success message when input captcha matches.', function(){
+    it('Should return success message when input captcha matchess.', function(){
         let surveyModel = new SurveyModel();
         let captcha = surveyModel.captcha;
         let result = surveyModel.verifyCaptchaInput(captcha);
